4afde12d00a10c45a07d604c42f61c3d01b73782
A-fandino <arnauf8@gmail.com>
2023-10-04 18:37:28 +0200

    Multiplication assembly program

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,59 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+@R2 // Reset R2 Value
+M=0
+
+//inc = R0
+@R0
+D=M
+@inc
+M=D
+
+//i=R1
+@R1
+D=M
+@i
+M=D
+
+//if (inc>=i) goto LOOP
+    @inc
+    D=M-D
+    @PRE_LOOP
+    D;JGE
+
+// else flip values
+    @i
+    D=M
+    @temp
+    M=D //temp = i
+
+    @inc
+    D=M
+    @i
+    M=D //i = inc
+
+    @temp
+    D=M
+    @inc
+    M=D //inc = temp
+
+(PRE_LOOP) //Skips the loop if the lowest value is zero
+@i
+D=M
+@END
+D;JLE
+(LOOP)
+    @inc
+    D=M
+    @R2
+    M=M+D
+    @i // Decrement the iterator
+    M=M-1
+    D=M
+    @LOOP // Loops stops when 'i' is 0
+    D;JGT
+
+(END)
+    @END
+    0;JMP

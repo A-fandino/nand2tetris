bb621ede147defc591ba36e93301fd42a97b6bc8
A-fandino <arnauf8@gmail.com>
2023-10-04 19:47:26 +0200

    Fill screen

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,52 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+    @KBD
+    D=M
+    @fill
+    M=-1
+    @START_PAINT
+    D;JNE
+    @fill
+    M=0
+    @START_PAINT
+    0;JMP
+
+(START_PAINT)
+    @fill
+    D=M
+    @SCREEN
+    D=M-D
+    @LOOP
+    D;JEQ
+    @SCREEN
+    D=M
+    @8192
+    D=A
+    @i
+    M=D
+    @FILL_SCREEN
+    0;JMP
+
+
+(FILL_SCREEN)
+    @SCREEN
+    D=A
+    @i
+    D=D+M
+    @pointer
+    M=D
+    @fill
+    D=M
+    @pointer
+    A=M
+    M=D
+    @i
+    M=M-1
+    D=M
+    @FILL_SCREEN
+    D;JGE
+    @LOOP
+    0;JMP

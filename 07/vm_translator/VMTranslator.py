0b9fe2f3c0dcf4355a3f214e635ed1433aa80a67
A-fandino <arnauf8@gmail.com>
2023-10-12 20:57:09 +0200

    Write basic vm parser

--- 07/vm_translator/VMTranslator.py
+++ 07/vm_translator/VMTranslator.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 
 
+from vm_parser import VmParser
+
+
 def main():
-    pass
+    with open("../MemoryAccess/BasicTest/BasicTest.vm", "r") as f:
+        lines = f.read()
+
+    parser = VmParser(lines)
+    while (line := parser.next_line()) is not None:
+        if not parser.is_instruction():
+            continue
+        print(parser.lineNumber, "-", line)
 
 
 if __name__ == "__main__":
